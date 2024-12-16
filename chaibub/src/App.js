0e0cd2bd42616deff0a2e3a702b967890a28ebda
rae8108 <rayanbana01@gmail.com>
2024-12-16 13:06:45 +0000

    started on navbar

--- chaibub/src/App.js
+++ chaibub/src/App.js
@@ -1,14 +1,18 @@
-import Nav from "./Nav/Nav"
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom'; 
+import Nav from "./Nav/Nav";
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <Nav />
-      </header>
+      <Router>
+        <Nav />
+      </Router>
     </div>
   );
+
 }
 
-export default App;
+
+
--- chaibub/src/Nav/Nav.js
+++ chaibub/src/Nav/Nav.js
@@ -1,8 +1,34 @@
-import React from "react";
-import "./Nav.css"
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
+import Home from "./Components/Home";
+import Menu from "./Components/Menu";
+import Products from "./Components/Products";
+import Locations from "./Components/Locations";
+import './Nav.css';
 
 export default function Nav() {
-    return (
-        <p>Navbar</p>
-    )
+  return (
+    <Router>
+      <div>
+        <div className="Nav">
+          <div>
+            <small className="name">chaibub</small>
+          </div>
+
+          <nav>
+            <div><NavLink to="/">Home</NavLink></div>
+            <div><NavLink to="/menu">Menu</NavLink></div>
+            <div><NavLink to="/products">Products</NavLink></div>
+            <div><NavLink to="/locations">Locations</NavLink></div>
+          </nav>
+        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />  {/* Modified to match NavLink */}
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/locations" element={<Locations />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
